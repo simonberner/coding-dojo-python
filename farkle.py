aabad8ed6c83a50abdcceb9b286c9bebcce50e56
Simon Berner <sibebzh@gmail.com>
2024-08-11 20:40:54 +0200

    refactor: :recycle: refactor logic for test#1
    Refactor the logic till test #1 passes. Consider the simplicity criteria.

--- farkle.py
+++ farkle.py
@@ -1,4 +1,12 @@
 class Farkle:
 
     def score(self, array_of_dice):
-        return 450
+        turn_score = 0
+        for dice in array_of_dice:
+            if dice == 1:
+                turn_score += 100
+            if dice == 5:
+                turn_score += 50
+        if array_of_dice.count(3) == 3:
+            turn_score += 300
+        return turn_score
