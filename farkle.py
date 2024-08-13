3467975ec753e3ec024aff56cc3f9f7906a5c533
Simon Berner <sibebzh@gmail.com>
2024-08-13 16:39:09 +0200

    feat(farkle): :sparkles: make the test#4 pass
    Make test4 and all the others pass by adding some simple logic.

--- farkle.py
+++ farkle.py
@@ -5,8 +5,17 @@ class Farkle:
         # check for triple dices
         turn_score = self.__score_triple_dice(array_of_dice, turn_score)
         # if no triple dice check single dice
-        if array_of_dice.count(1) != 3:
-            for dice in array_of_dice:
+        if array_of_dice.count(1) == 3:
+            new_dice_array = self.__remove_dice(1, array_of_dice)
+            for dice in new_dice_array:
+                turn_score = self.__score_single_dice(dice, turn_score)
+        if array_of_dice.count(4) == 3:
+            new_dice_array = self.__remove_dice(4, array_of_dice)
+            for dice in new_dice_array:
+                turn_score = self.__score_single_dice(dice, turn_score)
+        if array_of_dice.count(3) == 3:
+            new_dice_array = self.__remove_dice(3, array_of_dice)
+            for dice in new_dice_array:
                 turn_score = self.__score_single_dice(dice, turn_score)
         return turn_score
 
@@ -19,6 +28,10 @@ class Farkle:
             turn_score += 300
         if array_of_dice.count(4) == 3:
             turn_score += 400
+        if array_of_dice.count(5) == 3:
+            turn_score += 500
+        if array_of_dice.count(6) == 3:
+            turn_score += 600
         return turn_score
 
     def __score_single_dice(self, dice, turn_score):
@@ -27,3 +40,8 @@ class Farkle:
         if dice == 5:
             turn_score += 50
         return turn_score
+
+    # Removes all dice from the provided array_of_dice
+    def __remove_dice(self, dice, array_of_dice):
+        if array_of_dice.count(dice) == 3:
+            return [x for x in array_of_dice if x != dice]
