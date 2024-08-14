add8d45c8763a0a3efbd25f73be3c0c073f1a693
Simon Berner <sibebzh@gmail.com>
2024-08-14 17:52:11 +0200

    refactor(farkle): :recycle: refactor score logic
    Extract method and merge remove dice code.

--- farkle.py
+++ farkle.py
@@ -4,14 +4,17 @@ class Farkle:
         turn_score = 0
         # check for triple dice
         turn_score = self.__score_triple_dice(array_of_dice, turn_score)
-        # remove triple dice and score single dice
-        triple_dice_to_check = [1, 2, 3, 4, 5, 6]
-        for dice_value in triple_dice_to_check:
-            if array_of_dice.count(dice_value) == 3:
-                array_of_dice = self.__remove_dice(dice_value, array_of_dice)
+        array_of_dice = self.__remove_triple_dice(array_of_dice)
         for dice in array_of_dice:
             turn_score = self.__score_single_dice(dice, turn_score)
         return turn_score
+
+    def __remove_triple_dice(self, array_of_dice):
+        triple_dice_to_check = [1, 2, 3, 4, 5, 6]
+        for dice_value in triple_dice_to_check:
+            if array_of_dice.count(dice_value) == 3:
+                array_of_dice = [x for x in array_of_dice if x != dice_value]
+        return array_of_dice
 
     def __score_triple_dice(self, array_of_dice, turn_score):
         if array_of_dice.count(1) == 3:
@@ -34,8 +37,3 @@ class Farkle:
         if dice == 5:
             turn_score += 50
         return turn_score
-
-    # Removes all dice from the provided array_of_dice
-    def __remove_dice(self, dice, array_of_dice):
-        if array_of_dice.count(dice) == 3:
-            return [x for x in array_of_dice if x != dice]
